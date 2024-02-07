af051c098a20dea54e26a7db73f28fdecb28f825
tkddnd010 <tkddnd010@naver.com>
2024-02-07 18:46:55 +0900

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import userRouter from './routers/user.router.js';
+import postsRouter from './routers/post.router.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('하이');
-});
+app.use('/user', userRouter);
+app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸습니다.');
---
+++ middlewares/authomiddleware.js
@@ -0,0 +1,7 @@
+import jwt from 'jsonwebtoken';
+import { prisma } from '../model/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default async function (req, res, next) {}
