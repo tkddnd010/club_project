861be50ebbaf9243cbbf1ef1e40d34781801ad36
tkddnd010 <tkddnd010@naver.com>
2024-02-07 18:14:14 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('하이');
+});
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸습니다.');
+});
