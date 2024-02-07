a7feef1dc02cd8686b934d8a9d4241e37415dc25
kimminjae <kimminjae999@naver.com>
2024-02-07 21:13:19 +0900

    1번째 푸시

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import userRouter from './routers/user.router.js';
 import postsRouter from './routers/post.router.js';
+import commentsRouter from './routers/comments.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,7 @@ app.use(cookieParser());
 
 app.use('/user', userRouter);
 app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸습니다.');
---
+++ routers/comments.router.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { prisma } from '../model/index.js';
+import authMiddleware from '../middlewares/authomiddleware.js';
+
+const router = express.Router();
+
+router.post(
+  '/posts/:postId/comments',
+  authMiddleware,
+  async (req, res, next) => {
+    const { postId } = req.params;
+    const { content } = req.body;
+    const { userId } = req.user;
+
+    const post = await prisma.posts.findFirst({ where: { postId: +postId } });
+    if (!post) return res.status(404).json({ message: '댓글이 없습니다.' });
+
+    const comment = await prisma.comments.create({
+      data: {
+        postId: +postId,
+        userId: +userId,
+        content: content,
+      },
+    });
+    return res.status(201).json({ data: comment });
+  }
+);
+
+export default router;
