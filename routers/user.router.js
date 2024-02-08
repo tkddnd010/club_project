0c8c973cf0b5f721cab5a245fef4629b0b2ab357
김성진 <jinni1226@naver.com>
2024-02-08 21:02:42 +0900

    프로필수정API완료

--- routers/user.router.js
+++ routers/user.router.js
@@ -74,14 +74,16 @@ router.post('/sign-in', async (req, res, next) => {
   return res.status(200).json({ message: '로그인에 성공하였습니다.' });
 });
 
+// 로그아웃 API
 router.delete('/sign-out', async (req, res, next) => {
   res.setHeader(
     'Set-Cookie',
-    'authorization=; path=/; expires=Thu, Jan 1970 00:00:00 GMT; httponly'
+    'accessToken=; path=/; expires=Thu, Jan 1970 00:00:00 GMT; httponly'
   );
   res.status(204).end();
 });
 
+// 프로필 조회 API
 router.get('/me', validateAccessToken, async (req, res, next) => {
   const { userId } = req.user;
   const user = await prisma.users.findFirst({
@@ -96,6 +98,26 @@ router.get('/me', validateAccessToken, async (req, res, next) => {
     },
   });
   return res.status(200).json({ data: user });
+});
+
+// 프로필 수정 API
+router.put("/me",validateAccessToken , async (req, res, next) => {
+  const { userId } = req.user;
+  const { name, interest, selfInfo } = req.body;
+  const user = await prisma.Users.findUnique({
+    where: { userId: +userId },
+  });
+
+  const updateduser = await prisma.Users.update({
+    where: { userId: +userId },
+    data: {
+      userId: +userId,
+      name,
+      interest,
+      selfInfo,
+    },
+  });
+  return res.status(200).json({ message : "수정이 완료되었습니다." });
 });
 
 export default router;
